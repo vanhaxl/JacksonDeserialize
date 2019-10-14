52f12fa6892a17e2026ebbb67e28f5af5bde8256
vn50ggs <vn50ggs@homeoffice.wal-mart.com>
2019-10-14 14:31:54 -0500

    initial commit

---
+++ src/main/java/com/example/jacksondeserialize/JacksonDeserialize/JacksonDeserializeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacksondeserialize.JacksonDeserialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonDeserializeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonDeserializeApplication.class, args);
+	}
+
+}
